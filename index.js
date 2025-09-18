1e62ad7dd99101f31e66f9011bfdbdc03a4d1f09
Jairo Ramos <jairo_2219@hotmail.com>
2025-09-17 21:00:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,6 @@
 import { AppRegistry, NativeModules, Platform } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
-import crashlytics from '@react-native-firebase/crashlytics'
-
-const globalErrorHandler = (error, isFatal) => {
-  if (__DEV__) {
-    console.error(error);
-  } else {
-    crashlytics().recordError(error); // Reporta a Firebase
-  }
-};
-
-if (global.ErrorUtils) {
-  global.ErrorUtils.setGlobalHandler(globalErrorHandler);
-}
-
 
 
 AppRegistry.registerComponent(appName, () => App)
